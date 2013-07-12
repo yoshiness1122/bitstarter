8c8e638984d8e928fc75cc0225b461e37c267f74
Ubuntu <ubuntu@ip-10-164-37-81.ec2.internal>
2013-07-12 00:31:22 +0000

    added world 3

--- web.js
+++ web.js
@@ -3,7 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2 !');
+  response.send('Hello World3 !');
+});
+
+
+fs = require('fs')
+var rFile = fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+  console.log(rFile);
 });
 
 var port = process.env.PORT || 5000;
