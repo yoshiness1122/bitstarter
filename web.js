decdb1ad169de190b2248d1bb3301c98f57b940b
Ubuntu <ubuntu@ip-10-164-37-81.ec2.internal>
2013-07-12 01:46:53 +0000

    world 5

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3 !');
+  response.send('Hello World5 !');
 });
 
 
@@ -12,9 +12,11 @@ var rFile = fs.readFile('index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
-  console.log(rFile);
+  console.log("working    " +data);
+//  console.log(rFile);
 });
+
+//console.log("this is PRINTING properly *******" + rFile + "***");
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
